71beb5b6b960d78069efc4e94f73104b7747045b
Kamil <k.betkowsky@gmail.com>
2025-10-28 22:41:51 +0100

    added Jpa

--- property-listings-backend/src/main/java/com/realestate/property_listings_backend/image/ImageRepository.java
+++ property-listings-backend/src/main/java/com/realestate/property_listings_backend/image/ImageRepository.java
@@ -1,4 +1,6 @@
 package com.realestate.property_listings_backend.image;
 
-public class ImageRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<PropertyImage, Long> {
 }
--- property-listings-backend/src/main/java/com/realestate/property_listings_backend/user/UserRepository.java
+++ property-listings-backend/src/main/java/com/realestate/property_listings_backend/user/UserRepository.java
@@ -1,4 +1,6 @@
 package com.realestate.property_listings_backend.user;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
