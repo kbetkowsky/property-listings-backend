64bf9082f533dc98b679d375175bf270757ce210
Kamil <k.betkowsky@gmail.com>
2025-10-29 22:25:14 +0100

    dummy frontend

--- property-listings-backend/src/main/java/com/realestate/property_listings_backend/dto/ValidationErrorResponse.java
+++ property-listings-backend/src/main/java/com/realestate/property_listings_backend/dto/ValidationErrorResponse.java
@@ -1,4 +1,22 @@
 package com.realestate.property_listings_backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Getter
+@Builder
 public class ValidationErrorResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+
+    private Map<String, String> validationErrors;
 }
